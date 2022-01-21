7b03f7bc85778c519355686695e7b6ec27f0c7a2
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-01-21 12:32:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -376,8 +376,8 @@ def handle_message(event):
             get_message = strTitle + '：\n資料筆數[ ' + str(intCount) + ' ]\n' + \
                             datNow  + '\n\n' + \
                             strTemp + '\n' + \
-                            '以上為有（不合格）品項滅火器\n\n'
-                            ' .................................. \n\n'
+                            '以上為有（不合格）品項滅火器\n\n' + \
+                            ' ==================================== \n\n'
 
             strSQL = 'SELECT [FE_TIME] ,[FE_EQNAME] ,[CL-A01] AS CLA01 ,[FE_NAME] ' \
                         ' FROM [toyo_web].[dbo].[VIEW_APP_FE_EQ_CHK_OK_List01] ' + \
