58862f2442f88bf49ce82c089ac3eb249f735d38
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-02-17 10:00:41 +0800

    Update config.py

--- config.py
+++ config.py
@@ -20,11 +20,11 @@ strEctorToken = 'rOvcCpmikeueH0xYaSLacH8ya3kBDCJPeySEWyjb6KC'
 # 智弘權杖：
 strJohnboToken = 'W9a3o7JtKinyfgBRf9oX3oFRIXvrcYKC5XC7yl5KCqV'
 # 冠伶權杖：
-strGwenToken = ''
+strGwenToken = 'iEzx2cl5okBWmUb8f2BAD23esOvUKRNmwBIp7sIFpWU'
 # 昆霖權杖：
 strKunToken = '7MQCitn5m7DACt46wnpXJOFVhiwVdRAhBMpolK74rbG'
 # 汶靜權杖：
-strJingToken = ''
+strJingToken = 'L2e5pzTfF3a22jFIiZ1tQ2VN9lSrTDZ7A0nO3jzhwpL'
 # 宜庭權杖：        
 strMichelleToken = 'GksGM2RNtFn3TKz55ajI6ovsJgDcE7AmxJxvgk44Sey'
 # 玉敏權杖：        
