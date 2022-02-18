4e45026b071b19488bcae5533eacd75687a9b3c6
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-02-18 09:07:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -941,7 +941,6 @@ def RS_DateTime_2_HHNN():
 def RS_Line_LOG(strLineName, strLineUserID, strLineMSG):
     #取得時間
     datDT = datetime.now()
-    strToday = datDT.strftime("%Y-%m-%d") 
     strDateTime = datDT.strftime("%Y-%m-%d %H:%M:%S")
 
     #寫入LOG
@@ -952,7 +951,7 @@ def RS_Line_LOG(strLineName, strLineUserID, strLineMSG):
         ms = MSSQL(host=GVstr254_host, port=GVstr254_port, user=GVstr254_user, pwd=GVstr254_pwd, db=GVstr254_TIM_DB)
         strSQL = ' INSERT INTO [TIM_DB].[dbo].[tblAPP_TYAD_LineLog] ' + \
                     ' (EX01, EX02, EX03, TXT01, EXDT01) ' + \
-                    ' VALUES (' + (strDateTime) + ',' + (strLineName) + ',' + (strLineUserID) + ',' + (strLineMSG) + ',' + strDateTime + ') '
+                    ' VALUES (' + (strDateTime) + ',' + (strLineName) + ',' + (strLineUserID) + ',' + (strLineMSG) + ',Convert(datetime, ' + strDateTime + ',111)) '
         resList = ms.RS_SQL_ExecNonQuery(strSQL)
         RS_Line_LOG = strTitle + '：寫入DB(WEB) BT OK!\n' + \
                         strDateTime  + '\n\n' + \
