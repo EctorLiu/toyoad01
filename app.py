87ab94c956e72dab63466d73b659d137dc7eff5e
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-02-18 13:58:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ strVer = '(M218)1320'
     # 切換SQL功能選擇：ON/OFF
 strSQL_FW_Switch = 'ON'
     # 切換同仁推播功能選擇：ON/OFF
-strPush_NotKeyWord2All_Switch = 'ON'
+strPush_NotKeyWord2All_Switch = 'OFF'
     # ***** ***** ***** ***** *****
 
     ##### 限制 ######
