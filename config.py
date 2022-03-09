61795197011d7b9babbfad4f33254b0f0a7102f7
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-03-09 17:55:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -13,27 +13,6 @@ strchannel_secret = os.environ.get("CHANNEL_SECRET")
     # ***** ***** ***** ***** *****
 
 
-    ##### 權杖資料 ######
-    # strEctorToken、strJohnboToken、strGwenToken、strKunToken、strMichelleToken
-# EctorLiu權杖：
-strEctorToken = 'rOvcCpmikeueH0xYaSLacH8ya3kBDCJPeySEWyjb6KC'
-# 智弘權杖：
-strJohnboToken = 'W9a3o7JtKinyfgBRf9oX3oFRIXvrcYKC5XC7yl5KCqV'
-# 冠伶權杖：
-strGwenToken = 'iEzx2cl5okBWmUb8f2BAD23esOvUKRNmwBIp7sIFpWU'
-# 昆霖權杖：
-strKunToken = '7MQCitn5m7DACt46wnpXJOFVhiwVdRAhBMpolK74rbG'
-# 汶靜權杖：
-strJingToken = 'L2e5pzTfF3a22jFIiZ1tQ2VN9lSrTDZ7A0nO3jzhwpL'
-# 宜庭權杖：        
-strMichelleToken = 'GksGM2RNtFn3TKz55ajI6ovsJgDcE7AmxJxvgk44Sey'
-# 玉敏權杖：        
-strMinToken = 'Z25DvEJjmkdubIEdjK4RUiIQvYiAtOvdG6HjdqHewWR'
-# MOMO權杖：        
-strMomoToken = 'fFmh2ao2ebztFgUT0VhOZDsGNN60CaxYtirCtkkl7Yj'
-    # ***** ***** ***** ***** *****
-
-
     ##### SQL ######
 import pymssql
 
