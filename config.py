d607fc802e78ec8300b0e9db484bfb72c3f8d76c
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-03-09 18:11:00 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,65 +12,6 @@ strchannel_secret = os.environ.get("CHANNEL_SECRET")
 # channel_secret = 'CHANNEL_SECRET'
     # ***** ***** ***** ***** *****
 
-
-    ##### SQL ######
-import pymssql
-
-GVstr254_host = '211.23.242.222'
-GVstr254_port = '2255'
-GVstr254_user = 'sa'
-GVstr254_pwd = '00000'
-GVstr254_TIM_DB = 'TIM_DB'
-
-class MSSQL:
-    def __init__(self, host, port, user, pwd, db):
-        self.host = host
-        self.port = port
-        self.user = user
-        self.pwd = pwd
-        self.db = db
-
-    def RS_SQL_GetConnect(self):
-        if not self.db:
-            raise(NameError,"沒有設定資料庫資訊")
-        self.conn = pymssql.connect(host=self.host, port=self.port, user=self.user, password=self.pwd, database=self.db, charset='utf8')
-        cur = self.conn.cursor()
-        if not cur:
-            raise(NameError,"連線資料庫失敗")
-            # strSQLCond = "Connect NG"
-            # return strSQLCond
-        else:
-            return cur
-            # strSQLCond = "Connect OK"
-            # return strSQLCond
-
-    def RS_SQL_ExecQuery(self, sql):
-        cur = self.RS_SQL_GetConnect()
-        cur.execute(sql)
-        resList = cur.fetchall()
-        self.conn.close()
-        return resList
-        # strSQLCond = "RS_SQL_ExecQuery OK"
-        # return strSQLCond
-
-    def RS_SQL_ExecNonQuery(self, sql):
-        cur = self.RS_SQL_GetConnect()
-        cur.execute(sql)
-        self.conn.commit()
-        self.conn.close()
-
-# sql語句中有中文的時候進行encode
-# insertSql = "insert into WeiBo([UserId],[WeiBoContent],[PublishDate]) values(1,'測試','2012/2/1')".encode("utf8")
-# 連線的時候加入charset設定資訊
-# pymssql.connect(host=self.host,user=self.user,password=self.pwd,database=self.db,charset='utf8')
-
-# conn = pymssql.connect(host='192.168.1.254', user='sa', password='00000')
-# conn = pymssql.connect(host='192.168.1.254', port=1433, user='sa',password='00000',database='TIM_DB',charset='utf8', as_dict=True)
-# conn = pymssql.connect('192.168.1.254','sa', '00000', 'TIM_DB')
-# conn = pymssql.connect('211.23.242.220','sa@211.23.242.220', 'Sql#dsc20170524', 'TIMHRDB')
-# cursor = conn.cursor(as_dict=True)
-    # ***** ***** ***** ***** *****
-
     ##### 關鍵字查詢 ######
 GVstrMoneyText = '公費使用狀況'
 
