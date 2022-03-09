706ac91d0088311494a0498a49f7492b7257d22c
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-03-09 17:55:54 +0800

    Create rm_initial.py

---
+++ rm_initial.py
@@ -0,0 +1,27 @@
+    ##### 權杖資料 ######
+    # strEctorToken、strJohnboToken、strGwenToken、strKunToken、strMichelleToken
+# EctorLiu權杖：
+strEctorToken = 'rOvcCpmikeueH0xYaSLacH8ya3kBDCJPeySEWyjb6KC'
+# 智弘權杖：
+strJohnboToken = 'W9a3o7JtKinyfgBRf9oX3oFRIXvrcYKC5XC7yl5KCqV'
+# 冠伶權杖：
+strGwenToken = 'iEzx2cl5okBWmUb8f2BAD23esOvUKRNmwBIp7sIFpWU'
+# 昆霖權杖：
+strKunToken = '7MQCitn5m7DACt46wnpXJOFVhiwVdRAhBMpolK74rbG'
+# 汶靜權杖：
+strJingToken = 'L2e5pzTfF3a22jFIiZ1tQ2VN9lSrTDZ7A0nO3jzhwpL'
+# 宜庭權杖：        
+strMichelleToken = 'GksGM2RNtFn3TKz55ajI6ovsJgDcE7AmxJxvgk44Sey'
+# 玉敏權杖：        
+strMinToken = 'Z25DvEJjmkdubIEdjK4RUiIQvYiAtOvdG6HjdqHewWR'
+# MOMO權杖：        
+strMomoToken = 'fFmh2ao2ebztFgUT0VhOZDsGNN60CaxYtirCtkkl7Yj'
+    # ***** ***** ***** ***** *****
+
+    ##### SQL參數 ######
+GVstr254_host = '211.23.242.222'
+GVstr254_port = '2255'
+GVstr254_user = 'sa'
+GVstr254_pwd = '00000'
+GVstr254_TIM_DB = 'TIM_DB'
+    # ***** ***** ***** ***** *****
