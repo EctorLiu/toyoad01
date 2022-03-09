eecfd1e8a372502db007e7c14ac02c39f4e334ac
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-03-09 16:56:43 +0800

    Update rf_string_02.py

--- rf_string_02.py
+++ rf_string_02.py
@@ -1,4 +1,22 @@
-    ##### 字串處理 ######
+    ##### INDEX ######
+# def RS_LEFT_String_StrNum(strTemp, intNum): 取字串左邊幾個字 => (ABCDE, 2)取(AB)
+# def RS_RIGHT_String_StrNum(strTemp, intNum): 取字串右邊幾個字 => (ABCDE, 2)取(DE)
+# def RS_RIGHT_String_NotLeftStrNum(strTemp, intNum): 不取字串左邊幾個字 => (ABCDE, 2)取(CDE)
+# def RS_MID_String_Start_StrNum(strTemp, intStart, intNum): 取字串中間開始幾個字 => (ABCDE, 2, 3)取(BCD)
+    # ***** ***** ***** ***** *****
+
+
+    ##### 程式區域 ######
+    ##### 程式區域 ######
+    ##### 程式區域 ######
+    ##### 程式區域 ######
+    ##### 程式區域 ######
+    ##### 程式區域 ######
+    ##### 程式區域 ######
+    ##### 程式區域 ######
+    ##### 程式區域 ######
+    ##### 程式區域 ######
+
 def RS_LEFT_String_StrNum(strTemp, intNum):
     return strTemp[:intNum]
 
