21f348a8c79ee1ad5508c50b471a180273a7f953
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-03-10 14:12:48 +0800

    Create ri_parameters_01.py

---
+++ ri_parameters_01.py
@@ -0,0 +1,11 @@
+    ##### 版本 ######
+    # 切換SQL功能選擇：ON/OFF
+GVstrSQL_FW_Switch = 'ON'
+    # 切換全部成員推播功能選擇：ON/OFF
+GVstrPush_NotKeyWord2All_Switch = 'OFF'
+    # ***** ***** ***** ***** *****
+
+    ##### 限制 ######
+GVintMaxLineMSGString = 4900
+GVintMaxItemString = 200
+    # ***** ***** ***** ***** *****
