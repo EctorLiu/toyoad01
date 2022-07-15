15b1d3d50f77fd6001a70830189fc714f4f2f7d8
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-07-15 09:24:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1010,18 +1010,18 @@ def handle_message(event):
         
     ##### 推播Line Notify內容 #####
     elif get_TYPE_message == 'TYAD_Collection01':
-        reply = ImageSendMessage(original_content_url = 'https://github.com/EctorLiu/toyoad01/raw/main/img/TYAD_C01.jpg', \
-                     preview_image_url = 'https://raw.githubusercontent.com/EctorLiu/toyoad01/raw/main/img/TYAD_C01.jpg')
+        reply = ImageSendMessage(original_content_url = 'https://github.com/EctorLiu/toyoad01/raw/main/img/TYAD_C01.png', \
+                     preview_image_url = 'https://raw.githubusercontent.com/EctorLiu/toyoad01/raw/main/img/TYAD_C01.png')
         line_bot_api.reply_message(event.reply_token,  reply)
 
     elif get_TYPE_message == 'TYAD_Collection02':
-        reply = ImageSendMessage(original_content_url = 'https://github.com/EctorLiu/toyoad01/raw/main/img/TYAD_C02.jpg', \
-                     preview_image_url = 'https://raw.githubusercontent.com/EctorLiu/toyoad01/raw/main/img/TYAD_C02.jpg')
+        reply = ImageSendMessage(original_content_url = 'https://github.com/EctorLiu/toyoad01/raw/main/img/TYAD_C02.png', \
+                     preview_image_url = 'https://raw.githubusercontent.com/EctorLiu/toyoad01/raw/main/img/TYAD_C02.png')
         line_bot_api.reply_message(event.reply_token,  reply)
 
     elif get_TYPE_message == 'TYAD_Collection03':
-        reply = ImageSendMessage(original_content_url = 'https://github.com/EctorLiu/toyoad01/raw/main/img/TYAD_C03.jpg', \
-                     preview_image_url = 'https://raw.githubusercontent.com/EctorLiu/toyoad01/raw/main/img/TYAD_C03.jpg')
+        reply = ImageSendMessage(original_content_url = 'https://github.com/EctorLiu/toyoad01/raw/main/img/TYAD_C03.png', \
+                     preview_image_url = 'https://raw.githubusercontent.com/EctorLiu/toyoad01/raw/main/img/TYAD_C03.png')
         line_bot_api.reply_message(event.reply_token,  reply)
 
     ##### 推播Line Notify內容 #####
